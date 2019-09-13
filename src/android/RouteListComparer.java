75a8f666f7afe5321766f6c62af81394eb0e7b5f
pgcan <prasoon.can@gmail.com>
2019-09-13 10:35:25 +0530

    fixed classname RouteListComparer

--- src/android/RouteListComparer.java
+++ src/android/RouteListComparer.java
@@ -4,7 +4,7 @@ import androidx.mediarouter.media.MediaRouter;
 
 import java.util.Comparator;
 
-public class RoutListComparer implements Comparator<MediaRouter.RouteInfo> {
+public class RouteListComparer implements Comparator<MediaRouter.RouteInfo> {
     public int compare(MediaRouter.RouteInfo left, MediaRouter.RouteInfo right) {
         return left.getName().compareTo(right.getName());
     }
